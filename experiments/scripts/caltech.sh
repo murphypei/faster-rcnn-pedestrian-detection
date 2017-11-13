3153cfba525a7409ef62cc256e3c584e92dd078d
ChaoPei <peichao0118@gmail.com>
2017-11-13 12:55:47 +0800

    add caltech shell script

---
+++ experiments/scripts/caltech.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=$1
+NET=$2
+NET_lc=${NET,,}
+DATASET=$3
+
+array=( $@ )                    # all args from command
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:3:$len}   # the index of 3:len(args) args
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+case $DATASET in
+    all)
+    # This is a very long and slow training schedule
+    # You can probably use fewer iterations and reduce the
+    # time to the LR drop (set in the solver to 350,000 iterations).
+    TRAIN_IMDB="caltech_all_trainval"
+   
+    PT_DIR="caltech"
+    ITERS=490000
+    ;;
+    reasonable)
+    # This is a very long and slow training schedule
+    # You can probably use fewer iterations and reduce the
+    # time to the LR drop (set in the solver to 350,000 iterations).
+    TRAIN_IMDB="caltech_reasonable_trainval"
+    TEST_IMDB="caltech_reasonable_test"
+   
+    PT_DIR="caltech"
+    ITERS=490000
+    ;;
+    person_class_only)
+    # This is a very long and slow training schedule
+    # You can probably use fewer iterations and reduce the
+    # time to the LR drop (set in the solver to 350,000 iterations).
+    TRAIN_IMDB="caltech_person_class_trainval"
+    TEST_IMDB="caltech_person_class_test"
+   
+    PT_DIR="caltech"
+    ITERS=490000
+    ;;
+  *)
+    echo "No dataset given"
+    exit
+    ;;
+esac
+
+LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/train_net.py --gpu ${GPU_ID} \
+  --solver models/${PT_DIR}/${NET}/faster_rcnn_end2end/solver.prototxt \
+  --weights data/imagenet_models/${NET}.v2.caffemodel \
+  --imdb ${TRAIN_IMDB} \
+  --iters ${ITERS} \
+  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+  ${EXTRA_ARGS}
+
+set +x
+NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
+set -x
